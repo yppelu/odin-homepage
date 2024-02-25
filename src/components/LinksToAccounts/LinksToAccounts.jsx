561c00a6b9116043f6b4cbbd35f2117d509061e8
yppelu <yppelu@gmail.com>
2024-02-25 15:42:42 +0700

    Create MyWork section

--- src/components/LinksToAccounts/LinksToAccounts.jsx
+++ src/components/LinksToAccounts/LinksToAccounts.jsx
@@ -3,14 +3,14 @@ import './LinksToAccounts.css';
 export default function LinksToAccounts() {
   return (
     <div className='links-to-accounts'>
-      <a className='links-to-accounts__link' href='https://github.com/' title='My gitHub' aria-label='My GitHub'>
-        <img src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg' alt='' aria-hidden />
+      <a className='links-to-accounts__link' href='https://github.com/' title='My GitHub' aria-label='My GitHub'>
+        <img src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg' alt='My GitHub' aria-hidden />
       </a>
       <a className='links-to-accounts__link' href='https://linkedin.com/' title='My LinkedIn' aria-label='My LinkedIn'>
-        <img src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-plain.svg' alt='' aria-hidden />
+        <img src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-plain.svg' alt='My LinkedIn' aria-hidden />
       </a>
       <a className='links-to-accounts__link' href='https://twitter.com/' title='My Twitter' aria-label='My Twitter'>
-        <img src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/twitter/twitter-original.svg' alt='' aria-hidden />
+        <img src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/twitter/twitter-original.svg' alt='My Twitter' aria-hidden />
       </a>
     </div>
   );
--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -1,5 +1,21 @@
+import './MyWork.css';
+
+import ProjectCard from './ProjectCard/ProjectCard.jsx';
+
 export default function MyWork() {
   return (
-    <section className='my-work'></section>
+    <section className='my-work-section'>
+      <h2 className='my-work-section__title'>My work</h2>
+      <div className='my-work-section__projects-container'>
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </div>
+    </section>
   );
 }
